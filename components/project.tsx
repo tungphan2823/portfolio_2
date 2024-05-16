bd6472ac41e11151bfee65b435e7ff1f969d95f9
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-16 05:04:35 +0300

    update project

--- components/project.tsx
+++ components/project.tsx
@@ -17,13 +17,30 @@ export default function Project() {
       });
       tl.to("#block2", { x: 100, duration: 5 })
         .to("#block3", { x: 200, duration: 5 })
-        .to("#block4", { x: 300, duration: 5 });
+        .to("#block4", { x: 300, duration: 5 })
+        .to("#block5", { x: 300, duration: 5 });
     });
   });
   return (
     <div id="block0" className=" text-xl lg:text-2xl xl:text-3xl p-16">
       <h1>PROJECTS</h1>
       <div id="block1" className="p-8 max-w-2xl">
+        <h1 className="font-Freeman font-bold">
+          <a
+            className="text-greenDeep"
+            href="https://github.com/tungphan2823/good-keyboards"
+          >
+            GoodSwitch* 
+          </a>{" "}
+          ○ 2024
+        </h1>
+        <div className="text-sm lg:text-xl">
+          {" "}
+          <p>E-commerce Website </p>
+          <p>TypeScript, React JS, Tailwind CSS, SQLite</p>
+        </div>
+      </div>
+      <div id="block2" className="p-8 max-w-2xl">
         <h1 className="font-Freeman font-bold">
           <a
             className="text-greenDeep"
@@ -39,7 +56,7 @@ export default function Project() {
           <p>TypeScript, React JS, Node JS, PostgreSQL ○ VAMK - Wartsila </p>
         </div>
       </div>
-      <div id="block2" className="p-8 max-w-2xl">
+      <div id="block3" className="p-8 max-w-2xl">
         <h1 className="font-Freeman font-bold">
           <a
             className="text-greenDeep"
@@ -50,9 +67,11 @@ export default function Project() {
           </a>
           ○ 2023
         </h1>
-        <p className="text-sm lg:text-xl">JavaScript, React JS, PostgreSQL ○ VAMK </p>
+        <p className="text-sm lg:text-xl">
+          JavaScript, React JS, PostgreSQL ○ VAMK{" "}
+        </p>
       </div>
-      <div id="block3" className="p-8 max-w-2xl">
+      <div id="block4" className="p-8 max-w-2xl">
         <h1 className="font-Freeman font-bold">
           <a
             className="text-greenDeep"
@@ -65,7 +84,7 @@ export default function Project() {
         <p className="text-sm lg:text-xl">Java ○ VAMK </p>
       </div>
 
-      <div id="block4" className="p-8 max-w-2xl">
+      <div id="block5" className="p-8 max-w-2xl">
         <h1 className="font-Freeman font-bold">
           <a
             className="text-greenDeep"
@@ -77,7 +96,6 @@ export default function Project() {
         </h1>
         <p className="text-sm lg:text-xl">HTML, CSS, JavaScript ○ VAMK </p>
       </div>
-      <div id="block5"></div>
     </div>
   );
 }
